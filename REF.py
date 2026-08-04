3fc160415c04c64bc57feacaaa9d22bd1f36903c
mikhoraw <mickodelatorre020607@gmail.com>
2026-08-04 21:54:00 +0800

    Add files via upload

---
+++ REF.py
@@ -0,0 +1,17 @@
+name = str("Micko")
+print(name)
+weight = int("65")
+print(weight)
+pounds = float("143.30")
+print(pounds)
+mode = bool("medium")
+print(mode)
+
+names = ("Micko Dela Torre")
+age = ("19")
+add = ("Don Ignacio Dimson, Lubao Pampanga")
+culor = ("black")
+
+print("Helllooo!! My name is " + names + ". I am " + age
+      + " years old" + ", I live in " + add + ", and my"
+      " favorite color is " + culor + ".")
